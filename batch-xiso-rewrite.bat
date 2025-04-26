27838cc7610e870d90ebd49e6c834c304edbfe85
ViviNoSmol <82992516+IgLocoXD@users.noreply.github.com>
2025-04-26 15:50:51 -0300

    Add files via upload

---
+++ batch-xiso-rewrite.bat
@@ -0,0 +1,24 @@
+@echo OFF
+echo:
+echo Batch XISO Rewrite v0.1
+echo:
+set choice=
+set /p choice=Delete old redump ISO files after extracting? (Y/[N]) 
+if not '%choice%'=='' set choice=%choice:~0,1%
+echo:
+echo Reading ISO files in current directory...
+dir /s /b *.iso > infile.txt
+echo:
+for /F "tokens=*" %%A in (infile.txt) do (
+
+	echo Rewriting: "%%A"
+	extract-xiso -r "%%A"
+	if /I '%choice%'=='Y' (
+		echo Deleting: "*%%A.old"
+		del "*%%A.old"
+	)
+)
+echo:
+del infile.txt
+echo Batch XISO rewrite complete.
+@echo ON
